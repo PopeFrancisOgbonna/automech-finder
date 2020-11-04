f4b461e518d58d05302b34d514de79ecf9395190
Popefrancis24 <francisogbonna24@gmail.com>
2020-11-05 00:37:40 +0100

    edited route

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
       <Router>
         <Switch>
           <Route path="/" component={HomePage} exact />
-          <Route path="/home" component={HomePage}/>
+          <Route path="/home" component={HomePage} exact/>
           <Route path="/login" exact component={Login}/>
           <Route path="/partner/login" exact component={PartnerLoginPage}/>
           <Route path="/register" exact component={SignUp}/>
